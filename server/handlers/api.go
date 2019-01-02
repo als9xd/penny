4385404a1e6cd06425d45a7a10c5c80cba58256a
Alex Schrimpf <als9xd@mst.edu>
2019-01-02 15:17:20 -0600

    Moved endpoints

--- server/handlers/api.go
+++ server/handlers/api.go
@@ -33,3 +33,59 @@ func GetThread(db *sqlx.DB,c *gin.Context) {
   }
   c.JSON(http.StatusOK,thread)
 }
+
+func GetThreads(db *sqlx.DB,c *gin.Context) {
+  threads := []models.Thread{}
+  err := db.Select(&threads,`
+    SELECT id,name FROM thread;
+  `)
+  if err != nil {
+    log.Fatal(err)
+  }
+  c.JSON(http.StatusOK,gin.H{"data":threads})
+}
+
+func CreateThread(db *sqlx.DB,c *gin.Context) {
+  var b models.Thread
+  err := c.ShouldBind(&b)
+  if err != nil {
+    c.JSON(http.StatusBadRequest,gin.H{
+      "error": gin.H{
+        "code": http.StatusBadRequest,
+        "message": err.Error(),
+      },
+    })
+    return
+  }
+  thread := models.Thread{}
+  err = db.Get(&thread,`
+    INSERT INTO thread (name)
+    VALUES ($1)
+    RETURNING id,name;
+  `,c.PostForm("name"))
+  if err != nil {
+    log.Fatal(err)
+  }
+  c.JSON(http.StatusOK, gin.H{"data":thread})
+}
+
+
+func DeleteThread(db *sqlx.DB,c *gin.Context) {
+  thread := models.Thread{}
+  err := db.Get(&thread,`
+    DELETE FROM thread WHERE id = $1 RETURNING id,name;
+  `,c.Param("id"))
+  if err == sql.ErrNoRows {
+    c.JSON(http.StatusNotFound,gin.H{
+      "error": gin.H{
+        "code": http.StatusNotFound,
+        "message": fmt.Sprintf("No thread with id '%s'",c.Param("id")),
+      },
+    })
+    return
+  }
+  if err != nil {
+    log.Fatal(err)
+  }
+  c.JSON(http.StatusOK, gin.H{"data":thread})
+}
